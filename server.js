fa734213e1ffe17d832c3b2683755dc55d6a2f86
Mackenzie Gray <mdkulen@gmail.com>
2022-08-03 19:23:40 +1000

    added dotenv npm package and updated gitignore

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+
+// require .env file 
+
+// comment plan to do 
+
+// link to database with mysql connection
+
+// inquirer question to start 
+
+// switch statement to lead to next question functions 
+    // functions for questions specific to categories with query to database
+
