95503179744c3722984986a76b7f6caa3e691ba6
Mackenzie Gray <mdkulen@gmail.com>
2022-08-07 17:04:43 +1000

    fixed viewAllEmployees function

--- models/dbUtils.js
+++ models/dbUtils.js
@@ -6,7 +6,14 @@ class dbQueryUtil {
     }
 
     viewAllEmployees() {
-        return this.connection.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.department_id, role.salary, employee.manager_id FROM employee INNER JOIN role ON employee.role_id=role.title,');
+        return this.connection.query(`SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager 
+        FROM employee e
+        LEFT JOIN role r
+          ON e.role_id = r.id
+        LEFT JOIN department d
+        ON d.id = r.department_id
+        LEFT JOIN employee m
+          ON m.id = e.manager_id`);
     }
     createNewEmployee(employee) {
         return this.connection.query('INSERT INTO employee SET ?', employee);
@@ -23,7 +30,10 @@ class dbQueryUtil {
 
 
     viewAllRoles() {
-        return this.connection.query('SELECT role.id, role.title, role.salary, department.name AS department FROM role INNER JOIN department ON role.department_id=department.id');
+        return this.connection.query(`SELECT role.id, role.title, role.salary, department.name AS department 
+        FROM role 
+        INNER JOIN department 
+        ON role.department_id=department.id`);
     }
     addRole(newRole) {
         return this.connection.query('INSERT INTO role SET ?', newRole);
